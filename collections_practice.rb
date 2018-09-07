9a7939078f3208777646297e44d3e99f7e04fb7f
Rachel Wyatt <rw126@evansville.edu>
2018-09-07 11:12:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
+def sort_array_asc(integers)
+  integers.sort
+end
 
+def sort_array_desc(integers)
+end
+
+def sort_array_char_count(string_array)
+end
+
+def swap_elements(array)
+end
+
+def reverse_array(string_array)
+end
+
+def find_a(string)
+end
+
+def sum_array(integer_array)
+end
+
+def add_s(word_array)
+end
