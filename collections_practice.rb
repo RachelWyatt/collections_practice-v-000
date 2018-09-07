c9cb67e0553fe358957f280e827527ac413bb9e6
Rachel Wyatt <rw126@evansville.edu>
2018-09-07 11:18:05 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,6 +15,15 @@ def sort_array_desc(integers)
 end
 
 def sort_array_char_count(string_array)
+  string_array.sort do |a,b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
 end
 
 def swap_elements(array)
